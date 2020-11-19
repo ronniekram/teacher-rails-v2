16ad5956e6baf5546462971e33b356ac5239e4ea
Arynn Boniface <arynn.boniface@gmail.com>
2020-11-19 15:57:10 -0500

    what else have i broken to do this?

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   has_many :lesson_plans
-  #has_many :subjects, through: :lesson_plans
+  has_many :subjects, through: :lesson_plans
   validates :title, :author, :publisher, presence: true
   validates :title, uniqueness: true
   validates :pages, presence: true, numericality: { greater_than_or_equal_to: 1 }
